cdc927ab9db09ea92cbcde09276b35821657cdce
rahulsinghrajput-dev <rahul14singhrajput@gmail.com>
2026-08-14 01:12:58 +0530

    add chai component in vite react app

--- 02-vite-react-app/src/App.jsx
+++ 02-vite-react-app/src/App.jsx
@@ -1,6 +1,9 @@
+import Chai from "./Chai"
+
 function App() {
   return (
     <>
+      <Chai/>
       <h1>Chai aur React with vite</h1>
     </>
   )
---
+++ 02-vite-react-app/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <>
+            <h2>Chai in React</h2>
+        </>
+    )
+}
+
+export default Chai;
